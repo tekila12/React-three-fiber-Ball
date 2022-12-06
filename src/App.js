e16b3313423276069f20678c1e24b291893a672b
Ajnur <radovic.ajnur96@gmail.com>
2022-12-06 11:31:16 +0100

    Orbital controls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useSpring } from "@react-spring/core";
+import { a } from "@react-spring/web";
 import './App.css';
+import Scene from "./Scene";
 
-function App() {
+
+export default function App() {
+
+  const [{ background, fill }, set] = useSpring(
+    { background: "#f0f0f0", fill: "#202020" },
+    []
+  );
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <a.main style={{ background }}>
+        <Canvas className="canvas" dpr={[1, 2]}>
+          <Scene setBg={set} />
+
+          <OrbitControls
+            enablePan={false}
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+        </Canvas>
+      
+      </a.main>
+     </>
   );
 }
 
-export default App;
+
---
+++ src/Scene.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Html} from "@react-three/drei"
+export default function Scene() {
+  return (
+    <>
+     <Html>
+    hello
+     </Html>
+    
+    </>
+  )
+}
